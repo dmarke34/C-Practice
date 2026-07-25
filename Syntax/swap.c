16f91cc92efef732c933267dd6e52579cf669e67
dmarke34 <aaron.aparicio34@gmail.com>
2026-07-24 21:20:53 -0700

    We grinding

---
+++ Syntax/swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main()
+{
+    int a = 5, b = 10;
+    printf("Before: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After: a = %d, b = %d\n", a, b);
+    return 0;
+}
